17313bc34e1c590793d30213c74064d3f2f7fbbf
lennart <lens@itu.dk>
2026-02-09 12:54:45 +0100

    add test load index correctly

--- tests/test_index.py
+++ tests/test_index.py
@@ -10,6 +10,9 @@ from faiss import read_index, write_index
 
 from retrieval.config import VECTOR_DIM
 
+TEST_PATH_1 = "data/testdata/test-index/test1.index"
+TEST_PATH_2 = "data/testdata/test-index/test2.index"
+
 @pytest.fixture
 def fake_model():
     fake_model = Mock()
@@ -18,8 +21,7 @@ def fake_model():
 
 @pytest.fixture
 def index():
-    test_path_1 = "data/testdata/test-index/test1.index"
-    return Index(VECTOR_DIM, test_path_1)
+    return Index(VECTOR_DIM, TEST_PATH_1)
 
 TEST_DOCS = [
         {"id": "a", "metadata": {"synthetic_phrase": "first"}},
@@ -59,10 +61,18 @@ def test_search_returns_correct_results(index, fake_model):
     assert results[1][0][1] == "b"
 
 def test_index_writes_correctly():
-    test_path_2 = "data/testdata/test-index/test2.index"
-    idx = Index(VECTOR_DIM, test_path_2)
+    idx = Index(VECTOR_DIM, TEST_PATH_2)
 
-    assert os.path.exists(test_path_2)
+    assert os.path.exists(TEST_PATH_2)
+
+def test_index_loaded_correctly(fake_model):
+    doc_ids, vectors = em.create_doc_embeddings(TEST_DOCS, fake_model)
+    idx = Index(VECTOR_DIM, TEST_PATH_1, False)
+
+    assert np.allclose(idx.get(0), vectors[0])
+    assert np.allclose(idx.get(1), vectors[1])
+
+
     
 
 
